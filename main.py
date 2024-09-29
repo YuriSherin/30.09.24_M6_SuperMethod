42fa96f174a38d430076ca8d6c2a557c6232c8db
YuriSherin <YuriSherin@mail.ru>
2024-09-30 02:48:56 +0700

    Create project

---
+++ main.py
@@ -0,0 +1,66 @@
+class Horse:
+    """Класс лошади"""
+    x_distance = None       # пройденный путь.
+    sound = None            # звук, который издаёт лошадь.
+
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
+        super().__init__()
+
+    def run(self, dx):
+        """Метод увеличивает x_distance на dx, где dx - изменение дистанции"""
+        if not isinstance(dx, (int, float)):
+            raise TypeError("Операнд должен иметь тип 'int' или 'float'")
+        else:
+            self.x_distance += dx
+
+class Eagle:
+    """Класс орла"""
+    y_distance = None       # высота полёта
+    sound = None            # звук, который издаёт орёл
+
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'   # звук, который издаёт орёл
+
+    def fly(self, dy):
+        """Метод увеличивает y_distance на dy, где dy - изменение дистанции"""
+        if not isinstance(dy, (int, float)):
+            raise TypeError("Операнд должен иметь тип 'int' или 'float'")
+        else:
+            self.y_distance += dy
+
+class Pegasus(Horse, Eagle):
+    """Класс Пегаса"""
+    def __init__(self):
+        super().__init__()
+
+    def move(self, dx, dy):     # где dx и dy изменения дистанции
+        """Метод запускает наследованные методы run и fly соответственно"""
+        if not isinstance(dx, (int, float)) and not isinstance(dy, (int, float)):
+            raise TypeError("Операнд должен иметь тип 'int' или 'float'")
+        else:
+            self.run(dx)
+            self.fly(dy)
+
+    def get_pos(self):
+        """Метод возвращает текущее положение пегаса в виде кортежа - (x_distance, y_distance)
+        в том же порядке."""
+        return self.x_distance, self.y_distance
+
+    def voice(self):
+        """Метод печатает значение унаследованного атрибута sound"""
+        print(self.sound)
+
+
+# Пример работы программы:
+p1 = Pegasus()
+print(Pegasus.mro())
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
